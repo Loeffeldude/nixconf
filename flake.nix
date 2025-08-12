8df28d8cf1777c6eee7415c7cc3cd0fed5776045
loeffel <kraetschmerni@gmail.com>
2025-08-12 14:34:45 +0200

    correct home path

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
 
-            home-manager.users.loeffel = import ./home.nix;
+            home-manager.users.loeffel = import ./home/home.nix;
           }
         ];
       };
