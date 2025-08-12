f96f970e2bfddbe880a84bf49540aff4f7d8e75d
loeffel <kraetschmerni@gmail.com>
2025-08-12 16:51:26 +0200

    neovim: trying some stuff

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,25 +1,62 @@
 { config, pkgs, lib, ... }: {
-  # i manage nvim over the normal nvim dir here
-  # because of lazyvi
-  # some day i might migrate
 
-  home.packages = with pkgs; [
-    neovim
-    wget
-    fd
-    luarocks-nix
-    imagemagick
-    ripgrep
-    # latex
-    zathura
-    biber
-    # for lazy packages
-    nodejs_latest
-  ];
+  config = let
+    nvim-treesitter = pkgs.vimPlugins.nvim-treesitter;
+    treesitterWithGrammars = nvim-treesitter.withAllGrammars;
 
-  home.file.".config/nvim" = {
-    source = ./nvim;
-    recursive = true;
+    treesitter-parsers = pkgs.symlinkJoin {
+      name = "treesitter-parsers";
+      paths = treesitterWithGrammars.dependencies;
+    };
+  in {
+
+    home.sessionVariables = {
+      EDITOR = "nvim";
+      MANPAGER = "nvim +Man!";
+    };
+
+    xdg.configFile."nvim/lua/parsers.lua".text = # lua
+      ''
+        vim.opt.runtimepath:append ("${treesitter-parsers}")
+      '';
+
+    programs.neovim = {
+      enable = true;
+      coc.enable = false;
+      withNodeJs = true;
+      withPython3 = true;
+      withRuby = true;
+
+      plugins = [ treesitterWithGrammars ];
+
+      extraPackages = with pkgs; [
+        marksman
+        nil
+        nixpkgs-fmt
+        rustup
+        lua-language-server
+        fzf
+        stylua
+        taplo
+        gnumake
+        zig
+        gcc
+        clang-tools
+        arduino-language-server
+        arduino-cli
+        coreutils
+        wget
+        fd
+        luarocks-nix
+        imagemagick
+        ripgrep
+        # latex
+        zathura
+        biber
+        # for lazy packages
+        nodejs_latest
+      ];
+    };
   };
-  home.sessionVariables = { EDITOR = "nvim"; };
+
 }
