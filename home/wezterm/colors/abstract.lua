e47b02ee946ddba4a8660b2953c174f0c161cd67
loeffel <kraetschmerni@gmail.com>
2025-08-12 15:27:28 +0200

    wezterm: add colors

---
+++ home/wezterm/colors/abstract.lua
@@ -0,0 +1,45 @@
+return {
+	foreground = "#D0D0D0", -- Soft gray
+	background = "#000000", -- True black
+
+	cursor_bg = "#C98B60", -- Muted warm amber
+	cursor_fg = "#000000",
+	cursor_border = "#C98B60",
+
+	selection_fg = "#000000",
+	selection_bg = "#44475a", -- Soft muted blue-gray
+
+	scrollbar_thumb = "#2C2F3A", -- Subtle contrast
+	split = "#1F222D",
+
+	ansi = {
+		"#000000", -- Black
+		"#B0756D", -- Red (desaturated warm)
+		"#5B8576", -- Green (dusty teal)
+		"#C4AA76", -- Yellow (muted gold)
+		"#5F6D89", -- Blue (slate blue)
+		"#A88BAE", -- Magenta (soft lavender-purple)
+		"#5A8E99", -- Cyan (dusty cyan)
+		"#D0D0D0", -- White
+	},
+	brights = {
+		"#1F222D", -- Bright black
+		"#D89A84", -- Bright red
+		"#7BAA94", -- Bright green
+		"#E6C48D", -- Bright yellow
+		"#8095AD", -- Bright blue
+		"#C0A7C9", -- Bright magenta
+		"#89C1CD", -- Bright cyan
+		"#E8E8E8", -- Bright white
+	},
+
+	copy_mode_active_highlight_bg = { Color = "#C98B60" },
+	copy_mode_active_highlight_fg = { Color = "#000000" },
+	copy_mode_inactive_highlight_bg = { Color = "#5B8576" },
+	copy_mode_inactive_highlight_fg = { Color = "#000000" },
+
+	quick_select_label_bg = { Color = "#C98B60" },
+	quick_select_label_fg = { Color = "#000000" },
+	quick_select_match_bg = { Color = "#5B8576" },
+	quick_select_match_fg = { Color = "#000000" },
+}
