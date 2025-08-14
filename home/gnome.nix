5b7ec0b6909bfba0f2dfe0b65bd0eb90a9c0e1c3
loeffel <kraetschmerni@gmail.com>
2025-08-14 23:07:49 +0200

    proper themes

--- home/gnome.nix
+++ home/gnome.nix
@@ -7,8 +7,10 @@
     "org/gnome/desktop/screensaver" = {
       "picture-uri" = "/home/loeffel/.background-image";
     };
-
-    "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Adwaita-dark";
+      color-scheme = "prefer-dark";
+    };
   };
   home.file.".background-image".source = ../media/nix-dark.png;
 
@@ -17,12 +19,15 @@
   qt = {
     enable = true;
     platformTheme = "gnome";
-    style = "adwaita-dark";
+    style = {
+      name = "Adwaita-dark";
+      package = pkgs.adwaita-qt;
+    };
   };
   gtk = {
     enable = true;
     theme = {
-      name = "adwaita-dark";
+      name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
   };
