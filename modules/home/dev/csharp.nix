fc4b8fecb663c29519511c0d9535ae8a10d023e3
loeffel <kraetschmerni@gmail.com>
2025-08-19 22:59:00 +0200

    csharp remove some packages

--- modules/home/dev/csharp.nix
+++ modules/home/dev/csharp.nix
@@ -4,15 +4,7 @@ let cfg = config.dev.csharp;
 
 in {
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      dotnet-sdk_7
-      dotnet-sdk_8
-      mono
-      nuget
-      nunit
-      xunit
-      msbuild
-    ];
+    home.packages = with pkgs; [ dotnet-sdk_7 dotnet-sdk_8 mono nuget ];
   };
 }
 
