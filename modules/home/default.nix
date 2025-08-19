11ba282ad2e4ec703b4932b04d1c772f0e32020e
loeffel <kraetschmerni@gmail.com>
2025-08-19 22:50:29 +0200

    merge correctly

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -13,8 +13,8 @@
       useUserPackages = true;
       backupFileExtension = "backup";
       extraSpecialArgs.flake-inputs = flake-inputs;
-      users.loeffel = (import ./home.nix) {
-        config = { apps.enable = lib.mkForce config.apps.enable; };
+      users.loeffel = (import ./home.nix) // {
+        apps.enable = lib.mkForce config.apps.enable;
       };
     };
   };
