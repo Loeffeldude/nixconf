6166238288720adfcaf7d3ca968f81c07c427aa6
loeffel <kraetschmerni@gmail.com>
2025-08-19 23:16:13 +0200

    csharp: add path

--- modules/home/dev/csharp.nix
+++ modules/home/dev/csharp.nix
@@ -3,7 +3,14 @@ with lib;
 let cfg = config.dev.csharp;
 
 in {
-  config =
-    mkIf cfg.enable { home.packages = with pkgs; [ dotnet-sdk_8 mono nuget ]; };
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ dotnet-sdk_8 mono nuget omnisharp-roslyn ];
+
+    home = {
+      sessionPath = [ "$HOME/.dotnet/tools" "${pkgs.omnisharp-roslyn}" ];
+      sessionVariables = { DOTNET_ROOT = "${pkgs.dotnet-sdk_8}"; };
+    };
+  };
+
 }
 
