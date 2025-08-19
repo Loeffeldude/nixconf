f603fd6f32c6c6983892ca2c47ea747acc69e387
loeffel <kraetschmerni@gmail.com>
2025-08-19 23:01:06 +0200

    remove dotnet 7

--- modules/home/dev/csharp.nix
+++ modules/home/dev/csharp.nix
@@ -3,8 +3,7 @@ with lib;
 let cfg = config.dev.csharp;
 
 in {
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [ dotnet-sdk_7 dotnet-sdk_8 mono nuget ];
-  };
+  config =
+    mkIf cfg.enable { home.packages = with pkgs; [ dotnet-sdk_8 mono nuget ]; };
 }
 
