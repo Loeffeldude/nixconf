a15bfd76685d628598e96c5dba00df5631fee803
Loeffeldude <kraetschmerni@gmail.com>
2025-08-21 13:21:29 +0200

    add ollama as service and package

--- modules/ai/ollama.nix
+++ modules/ai/ollama.nix
@@ -5,18 +5,18 @@ let cfg = config.ai.ollama;
 in {
 
   config = mkIf cfg.enable {
-
-
     environment.systemPackages = [
       (pkgs.ollama.override {
         acceleration =
           if cfg.nvidia then "cuda" else "rocm";
       })
     ];
-    # services.ollama = { enable = true;
-    #   acceleration =
-    #     if cfg.nvidia then "cuda" else "rocm";
-    #   loadModels = [ "gpt-oss:20b" "deepseek-r1:8b" ];
-    # };
+
+    services.ollama = {
+      enable = true;
+      acceleration =
+        if cfg.nvidia then "cuda" else "rocm";
+      loadModels = [ "gpt-oss:20b" "deepseek-r1:8b" ];
+    };
   };
 }
