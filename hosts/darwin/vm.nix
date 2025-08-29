bdb9614a1b364e2469568fcbeba99d0396665e59
Loeffeldude <kraetschmerni@gmail.com>
2025-08-29 22:15:50 +0200

    test

--- hosts/darwin/vm.nix
+++ hosts/darwin/vm.nix
@@ -10,7 +10,7 @@
   nix.settings.experimental-features = "nix-command flakes";
 
   # Set Git commit hash for darwin-version.
-  system.configurationRevision = self.rev or self.dirtyRev or null;
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowBroken = true;
