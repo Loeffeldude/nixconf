1b6ee7479e402e017f5f70aa80b67ab3c1d9202d
Loeffeldude <kraetschmerni@gmail.com>
2026-02-25 10:20:34 +0100

    go lsp

--- modules/nixos/virtualization/default.nix
+++ modules/nixos/virtualization/default.nix
@@ -6,15 +6,30 @@
       package = pkgs.qemu_full;
       runAsRoot = true;
       swtpm.enable = true;
+      verbatimConfig = ''
+        user = "root"
+        group = "root"
+        cgroup_device_acl = [
+          "/dev/null", "/dev/full", "/dev/zero",
+          "/dev/random", "/dev/urandom",
+          "/dev/ptmx", "/dev/kvm",
+          "/dev/dri/renderD128"
+        ]
+      '';
     };
   };
   virtualisation.spiceUSBRedirection.enable = true;
-  users.users.${config.primaryUser}.extraGroups = [ "libvirtd" "qemu-libvirtd" ];
+  users.users.${config.primaryUser}.extraGroups = [ "libvirtd" "qemu-libvirtd" "render" ];
   environment.systemPackages = with pkgs; [
     OVMF
+    mesa
+    virglrenderer
   ];
   boot.kernel.sysctl."net.ipv4.ip_forward" =
     1; # 1. Enable IP forwarding (required for NAT)
   networking.firewall.trustedInterfaces =
     [ "virbr0" ]; # 2. Tell firewall to trust the libvirt bridge
+  
+  # Enable graphics for VirtIO-GPU
+  hardware.graphics.enable = true;
 }
