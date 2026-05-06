ae16c7c385d04dc5627074f80bdf9766b7be7732
Loeffeldude <kraetschmerni@gmail.com>
2026-05-06 10:47:37 +0200

    feat: wire plasma x11 session for i3

--- modules/nixos/desktop/kde/default.nix
+++ modules/nixos/desktop/kde/default.nix
@@ -1,10 +1,13 @@
 { config, pkgs, lib, ... }:
 with lib;
 
-let cfg = config.desktop.kde;
-in {
-
-  options.desktop.kde = { enable = mkEnableOption "enable kde with i3"; };
+let
+  cfg = config.desktop.kde;
+in
+{
+  options.desktop.kde = {
+    enable = mkEnableOption "enable kde with i3";
+  };
 
   config = mkIf cfg.enable {
     services = {
@@ -15,45 +18,52 @@ in {
           variant = "";
           options = "caps:escape";
         };
-        
+
         windowManager.i3 = {
           enable = true;
           extraPackages = with pkgs; [
-            dmenu
-            i3status
             i3lock
+            i3status
           ];
         };
       };
 
       desktopManager.plasma6.enable = true;
+      displayManager.defaultSession = "plasmax11";
 
       displayManager.sddm = {
         enable = true;
         wayland.enable = false;
       };
+
+      libinput = {
+        enable = true;
+        touchpad = {
+          tapping = true;
+          naturalScrolling = true;
+          middleEmulation = true;
+          accelProfile = "adaptive";
+        };
+      };
+
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+
+      power-profiles-daemon.enable = true;
+      upower.enable = true;
     };
 
-    # Sound
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
 
-    # Power management
-    services.power-profiles-daemon.enable = true;
-    services.upower.enable = true;
-
-    # Bluetooth
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
     };
 
-    # Network Manager
     networking.networkmanager = {
       enable = true;
       wifi.powersave = true;
@@ -61,10 +71,20 @@ in {
 
     environment.systemPackages = with pkgs; [
       feh
+      picom
+      wmctrl
       xclip
+      xorg.xprop
+      xorg.xwininfo
+      networkmanagerapplet
+      blueman
+      pavucontrol
+      kdePackages.spectacle
+      playerctl
     ];
 
-    home-manager.users.${config.primaryUser} = { desktop.kde.enable = true; };
+    home-manager.users.${config.primaryUser} = {
+      desktop.kde.enable = true;
+    };
   };
 }
-
