41e743646c94752df0f71ac242ba624fe79dd86f
Nico Krätschmer <n.kraetschmer@linkvertise.com>
2026-06-29 08:17:48 +0200

    nix fix

--- modules/home/shared/dev/k8s.nix
+++ modules/home/shared/dev/k8s.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, flake-inputs, ... }:
 with lib;
-let cfg = config.dev.k8s;
-
-in {
+let
+  cfg = config.dev.k8s;
+  stablePkgs = import flake-inputs.nixpkgs-stable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in
+{
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       kubectl
       minikube
-      kubernetes-helm
+      # broken in unstable sha256-BASnpCLodmgiVn0M1MU2Pqyoz0aHwar/0qLkp7CjvSQ=
+      stablePkgs.kubernetes-helm
       kubectx
       k9s
       awscli2
